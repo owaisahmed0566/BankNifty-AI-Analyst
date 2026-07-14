8ef4334f0e3c2a73c985f32036f578d434588f8a
owaisahmed0566 <owaisahmed0566@gmail.com>
2026-07-14 21:39:42 +0400

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import ta
 
 
@@ -6,45 +5,69 @@ def calculate_indicators(data):
 
     df = data.copy()
 
-    # EMA
-    df["EMA_9"] = ta.trend.ema_indicator(
-        df["Close"], window=9
-    )
 
     df["EMA_20"] = ta.trend.ema_indicator(
-        df["Close"], window=20
+        df["Close"],
+        window=20
     )
+
 
     df["EMA_50"] = ta.trend.ema_indicator(
-        df["Close"], window=50
+        df["Close"],
+        window=50
     )
 
-    df["EMA_200"] = ta.trend.ema_indicator(
-        df["Close"], window=200
-    )
 
-    # RSI
     df["RSI"] = ta.momentum.rsi(
-        df["Close"], window=14
+        df["Close"],
+        window=14
     )
 
-    # MACD
+
     df["MACD"] = ta.trend.macd(
         df["Close"]
     )
 
-    # ATR
-    df["ATR"] = ta.volatility.average_true_range(
-        df["High"],
-        df["Low"],
-        df["Close"]
-    )
 
-    # VWAP
     df["VWAP"] = (
         (df["Close"] * df["Volume"]).cumsum()
         /
         df["Volume"].cumsum()
     )
 
-    return df
+
+    latest = df.iloc[-1]
+
+
+    summary = {
+
+        "Price":
+        round(float(latest["Close"]),2),
+
+        "EMA20":
+        round(float(latest["EMA_20"]),2),
+
+        "EMA50":
+        round(float(latest["EMA_50"]),2),
+
+        "RSI":
+        round(float(latest["RSI"]),2),
+
+        "MACD":
+        round(float(latest["MACD"]),2),
+
+        "VWAP":
+        round(float(latest["VWAP"]),2)
+
+    }
+
+
+    return df, summary
+
+
+
+if __name__ == "__main__":
+
+    print(
+        "Indicator Engine Ready"
+    )
